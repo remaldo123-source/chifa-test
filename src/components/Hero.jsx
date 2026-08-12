d1dbf803e6e6c12ff480dc91570832cfa5cd0bcd
Renzo <REMALDO123@GMAIL.COM>
2026-08-12 18:56:48 -0500

    Initial commit: chifa

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -43,8 +43,10 @@ export default function Hero() {
 
           {/* Headline */}
           <h1 className="proun-text font-display font-black uppercase text-6xl sm:text-7xl lg:text-[6.5rem] leading-[0.9] tracking-tight text-white">
-            <span className="block">SABOR QUE</span>
-            <span className="block text-[#D6A62A]">UNE CULTURAS</span>
+            <span className="block">EL SABOR</span>
+            <span className="block">QUE</span>
+            <span className="block">SIEMPRE</span>
+            <span className="block text-[#D6A62A]">GANAS</span>
           </h1>
 
           {/* Subtitle */}
